eb7c677bdf243187e10e3973da3313b6bd48b55e
mahesh jamdade <maheshmn121@gmail.com>
2022-12-04 14:25:40 -0500

    Day 4 part 2

--- solutions/day04.dart
+++ solutions/day04.dart
@@ -24,22 +24,18 @@ class Day04 extends GenericDay {
     return count;
   }
 
-// .2345678.  2-8
-// ..34567..  3-7
-
-// .....6...  6-6
-// ...456...  4-6
-
-// .23456...  2-6
-// ...45678.  4-8
-
-// ..23456...  2-6
-// .12345678.  1-8
-
   @override
   int solvePart2() {
-
-    return 0;
+    final assignments = parseInput();
+    int count =0;
+    for(int i=0;i< assignments.length-1;i++){
+      final assignment = assignments[i].split(RegExp(r'[,-]')).map((e) => int.parse(e)).toList();
+      if(!(assignment[0] < assignment[2] && assignment[1] < assignment[2]) &&
+         !(assignment[0] > assignment[3] && assignment[0] > assignment[3])){
+        count+=1;
+      }
+    }
+    return count;
   }
 }
 
