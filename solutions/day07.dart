a840f62a99cc4d7fb8aa7c484dd242ad9f5f49ce
mahesh jamdade <maheshmn121@gmail.com>
2022-12-17 17:10:57 -0500

    day 7 puzzle 2

--- solutions/day07.dart
+++ solutions/day07.dart
@@ -1,8 +1,8 @@
 import '../utils/index.dart';
 
 class Day07 extends GenericDay {
-  String? dirPath = '/';
   List<String> dirStack = [];
+  Map<String, int> dirStructure = {};
   Day07() : super(7);
 
   @override
@@ -21,33 +21,34 @@ class Day07 extends GenericDay {
       if (command.contains('\$')) {
         if (command.contains('cd /')) {
           dirStack = ['root'];
-          dirPath = getCurrentDirectory();
-          dirSize[dirPath!] = 0;
         } else if (command.contains(RegExp('cd [a-z]+'))) {
           changeDirectory(command);
         } else if (command.contains('cd ..')) {
-          String path = getCurrentDirectory();
-          int size = dirSize[path] ?? 0;
           changeDirectoryAStepBack();
-
-          /// todo add size to parent size
-          String parent = getCurrentDirectory();
-          dirSize[parent] = (dirSize[parent] ?? 0) + size;
         }
-      } else
-      // list files or dir in pwd
-      if (command.contains(RegExp(filePattern))) {
-        String size = command.split(' ').toList()[0];
-        int fileSize = int.parse(size);
-        String current = getCurrentDirectory();
-        dirSize[current] = (dirSize[current] ?? 0) + fileSize;
-      } else if (command.contains(RegExp('dir [a-z]+'))) {
-        String dirName = command.getDirName(index: 1);
-        String path = getCurrentDirectory() + '/' + dirName;
-        dirSize[path] = 0;
+      } else {
+        // list files or dir in pwd
+        if (command.contains(RegExp(filePattern))) {
+          String size = command.split(' ').toList()[0];
+          int fileSize = int.parse(size);
+          String current = getCurrentDirectory();
+          dirSize[current] = (dirSize[current] ?? 0) + fileSize;
+          if (current != 'root') {
+            dirSize['root'] = (dirSize['root'] ?? 0) + fileSize;
+            String parent = getParentDirectory();
+            if (parent != 'root') {
+              /// todo add size to parent size
+              dirSize[parent] = (dirSize[parent] ?? 0) + fileSize;
+            }
+          }
+        } else if (command.contains(RegExp('dir [a-z]+'))) {
+          String dirName = command.getDirName(index: 1);
+          String path = getCurrentDirectory() + '/' + dirName;
+          dirSize[path] = 0;
+        }
       }
-      print(dirSize);
     }
+    dirStructure = dirSize;
     int total = 0;
     for (String key in dirSize.keys) {
       int size = dirSize[key] ?? 0;
@@ -63,27 +64,40 @@ class Day07 extends GenericDay {
   // /a/b/c
   void changeDirectoryAStepBack() {
     dirStack.removeLast();
-    print('current dir ${getCurrentDirectory()}}');
   }
 
   // $ cd dirName
   void changeDirectory(String command) {
     String dirName = command.getDirName(index: 2);
-    if (dirStack.isEmpty) {
-      print("this will never execute");
-      dirStack.add('root');
-    }
     dirStack.add(dirName);
-    print('current dir: ${getCurrentDirectory()}');
   }
 
   String getCurrentDirectory() {
     return dirStack.join('/');
   }
 
+  String getParentDirectory() {
+    final temp = dirStack.toList();
+    temp.removeLast();
+    return temp.join('/');
+  }
+
   @override
   int solvePart2() {
-    return 0;
+    int totalSpace = 70000000;
+    int unusedSpace = totalSpace - dirStructure['root']!;
+    int requiredSpace = 30000000 - unusedSpace;
+    int foundSize = totalSpace;
+    String directoryName = '';
+    for (String path in dirStructure.keys) {
+      int size = dirStructure[path] ?? 0;
+      if (size < foundSize && size > requiredSpace) {
+        foundSize = size;
+        directoryName = path.split('/').last;
+      }
+    }
+    print('directoryName: $directoryName');
+    return foundSize;
   }
 }
 
@@ -108,30 +122,3 @@ class Directory {
   int size;
   Directory(this.name, this.files, this.directories, {this.size = 0});
 }
-
-
-/*
-$ cd /  => create a root directory with name / and currentDir = ['/',[],[]]
-$ ls    => skip
-dir a   => create a directory with name "a", currentDir = ['/',[a],[]]
-14848514 b.txt  => add a file to current directory and increase size ['/',[a], "b.txt" ] 
-8504156 c.dat  => add a file to current directory and increase size ['/',[a],["b.txt","c.dat"]
-dir d  => create a directory with name "d" currentDir=['/',[a,d],["b.txt","c.dat"]]
-$ cd a => change current directory to "a" currentDir = ["/a",[],[]]
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k
-*/
