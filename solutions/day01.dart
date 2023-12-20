9bc56b4775cc7b2a1831062ba7c6133f5c0621a3
mahesh jamdade <maheshmn121@gmail.com>
2023-12-20 14:06:40 -0500

    day 1 part 1

--- solutions/day01.dart
+++ solutions/day01.dart
@@ -1,39 +1,48 @@
 import '../utils/index.dart';
 
-/// Every day should extend [GenericDay] to have access to the corresponding
-/// input and a common interface.
-///
-/// Naming convention is set to pad any single-digit day with `0` to have proper
-/// ordering of files and correct mapping between input for days and the day
-/// files.
 class Day01 extends GenericDay {
-  // call the superclass with an integer == today´s day
   Day01() : super(1);
 
-  /// The [InputUtil] can be accessed through the superclass variable `input`. \
-  /// There are several methods in that class that parse the input in different
-  /// ways, an example is given below
-  ///
-  /// The return type of this is `dynamic` for [GenericDay], so you can decide
-  /// on a day-to-day basis what this function should return.
   @override
-  List<int> parseInput() {
-    final lines = input.getPerLine();
-    // exemplary usage of ParseUtil class
-    return ParseUtil.stringListToIntList(lines);
+  List<String> parseInput() {
+    return input.getPerLine();
   }
 
-  /// The `solvePartX` methods always return a int, the puzzle solution. This
-  /// solution will be printed in main.
+  int getNumber(String input) {
+    var number = 0;
+    const factor = 10;
+    for (final char in input.split('')) {
+      if (char.codeUnitAt(0) >= 48 && char.codeUnitAt(0) <= 57) {
+        number = number * factor + int.parse(char);
+      }
+    }
+    return number;
+  }
+
+  int firstDigit(int n) {
+    var x = n;
+    while (x >= 10) {
+      x = x ~/ 10;
+    }
+    return x;
+  }
+
   @override
   int solvePart1() {
-    // TODO implement
-    return 0;
+    final inputs = parseInput();
+    var sum = 0;
+    for (final input in inputs) {
+      final out = getNumber(input);
+      final first = firstDigit(out);
+      final last = out % 10;
+      final result = first * 10 + last;
+      sum += result;
+    }
+    return sum;
   }
 
   @override
   int solvePart2() {
-    // TODO implement
     return 0;
   }
 }
